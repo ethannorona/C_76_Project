49ef469ea19a7db55428b6fc302871de39164e56
ethannorona <77581569+ethannorona@users.noreply.github.com>
2021-06-12 21:04:07 -0400

    Add files via upload

---
+++ screens/StarMap.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class StarMapScreen extends React.Component{
+  render(){
+    return(
+      <View style = {{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <Text>Star Map Screen</Text>
+      </View>
+    );
+  }
+}
